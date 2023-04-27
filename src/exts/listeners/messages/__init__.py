4930d09918326e63e66a7003fca2e3b1fffb5bf5
JacobMonck <jacobamonck@gmail.com>
2023-04-27 23:58:24 +0000

    refactor: organize listeners in their respective directory

---
+++ src/exts/listeners/messages/__init__.py
@@ -0,0 +1,9 @@
+from .on_bulk_message_delete import OnBulkMessageDelete
+from .on_message import OnMessage
+from .on_message_delete import OnMessageDelete
+
+__all__ = (
+    "OnMessage",
+    "OnMessageDelete",
+    "OnBulkMessageDelete",
+)
